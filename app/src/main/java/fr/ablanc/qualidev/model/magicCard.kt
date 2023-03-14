b6d21c9d447b61116469063e57c7ce0f19e530d6
Alexis <bzdalexis@gmail.com>
2023-03-14 11:36:22 +0100

    move to packages

--- app/src/main/java/fr/ablanc/qualidev/model/magicCard.kt
+++
@@ -1,9 +0,0 @@
-package fr.ablanc.qualidev.model
-
-data class magicCard(
-    var nommagicCard: magicCard,
-    var prixMagicCard: magicCard,
-    var forceCard: magicCard,
-    var typeMagicCard: magicCard,
-    var rareteMagicCard: magicCard
-)
--- app/src/main/java/fr/ablanc/qualidev/model/pokemonCard.kt
+++
@@ -1,9 +0,0 @@
-package fr.ablanc.qualidev.model
-
-data class pokemonCard(
-    var pvpokemonCard: pokemonCard,
-    var namePokemon: pokemonCard,
-    var typePokemon: pokemonCard,
-    var attaquePokemon: pokemonCard,
-    var rarete: pokemonCard
-)
