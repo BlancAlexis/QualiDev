d6c1b9141b983c2c840056093133953d4223b037
BLANC ALEXIS p2110038 <alexis.blanc@etu.univ-lyon1.fr>
2023-03-14 11:06:54 +0100

    add data class for pokemon and magic card

---
+++ app/src/main/java/fr/ablanc/qualidev/model/magicCard.kt
@@ -0,0 +1,9 @@
+package fr.ablanc.qualidev.model
+
+data class magicCard(
+    var nommagicCard: magicCard,
+    var prixMagicCard: magicCard,
+    var forceCard: magicCard,
+    var typeMagicCard: magicCard,
+    var rareteMagicCard: magicCard
+)
---
+++ app/src/main/java/fr/ablanc/qualidev/model/pokemonCard.kt
@@ -0,0 +1,9 @@
+package fr.ablanc.qualidev.model
+
+data class pokemonCard(
+    var pvpokemonCard: pokemonCard,
+    var namePokemon: pokemonCard,
+    var typePokemon: pokemonCard,
+    var attaquePokemon: pokemonCard,
+    var rarete: pokemonCard
+)
