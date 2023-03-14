c68ac95bb1a9368e39c0b322d92aadb010e08599
Alexis <bzdalexis@gmail.com>
2023-03-14 11:35:54 +0100

    split into pokemon & magic packages

---
+++ app/src/main/java/fr/ablanc/qualidev/model/magic/MagicCard.kt
@@ -0,0 +1,9 @@
+package fr.ablanc.qualidev.model.magic
+
+data class magicCard(
+        var nommagicCard: magicCard,
+        var prixMagicCard: magicCard,
+        var forceCard: magicCard,
+        var typeMagicCard: magicCard,
+        var rareteMagicCard: magicCard
+)
