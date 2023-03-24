00f29e146205c75b99ead258cead369be99b9c9d
Alexis <bzdalexis@gmail.com>
2023-03-24 14:16:23 +0100

    change name of variable on MagicCard Class

--- app/src/main/java/fr/ablanc/qualidev/model/magic/MagicCard.kt
+++ app/src/main/java/fr/ablanc/qualidev/model/magic/MagicCard.kt
@@ -3,14 +3,14 @@ package fr.ablanc.qualidev.model.magic
 import fr.ablanc.qualidev.model.Card
 
 class MagicCard(
-        var prixMagicCard: Int,
-        var forceCard: Int,
-        var typeMagicCard: String,
-        var rareteMagicCard: String,
+        var price: Int,
+        var strength: Int,
+        var type: String,
+        var rarity: String,
         name: String,
         cardType: String
 ) : Card(name, cardType) {
         override fun toString(): String {
-                return "MagicCard(prixMagicCard=$prixMagicCard, forceCard=$forceCard, typeMagicCard='$typeMagicCard', rareteMagicCard='$rareteMagicCard')"
+                return "MagicCard(prixMagicCard=$price, forceCard=$strength, typeMagicCard='$type', rareteMagicCard='$rarity')"
         }
 }
