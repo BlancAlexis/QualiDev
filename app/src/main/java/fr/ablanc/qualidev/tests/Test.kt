280a0341432d407763843897a83d7eee9ac47e7e
Alexis <bzdalexis@gmail.com>
2023-04-04 11:48:24 +0200

    update magic init with zero test && add pokemon attack test

--- app/src/main/java/fr/ablanc/qualidev/tests/Test.kt
+++ app/src/main/java/fr/ablanc/qualidev/tests/Test.kt
@@ -2,10 +2,11 @@ package fr.ablanc.qualidev.tests
 
 import android.util.Log
 import fr.ablanc.qualidev.model.magic.Attack
+import fr.ablanc.qualidev.utils.InitWithZeroException
 
 class Test {
     fun testMagicAttackInit(){
-        val attack : Attack = Attack(10,15,"condition")
+        val attack = Attack(10,15,"condition")
         assert(attack.getCost() == 10)
         assert(attack.getDamage() == 15)
         assert(attack.getCondition() == "condition")
@@ -13,15 +14,37 @@ class Test {
     }
 
     fun testMagicAttackInitWithZero(){
-        val attackWithZeroParams : Attack = Attack(0,0,"")
-        assert(attackWithZeroParams.getCost() == 0)
-        assert(attackWithZeroParams.getDamage() == 0)
-        assert(attackWithZeroParams.getCondition() == "")
-        Log.d("TEST", "testMagicAttackInitWithZero done");
+        try {
+            val attackWithZeroParams = Attack(0,0,"")
+        }catch (e : InitWithZeroException) {
+            Log.d("TEST", "testMagicAttackInitWithZero done")
+            return
+        }
+        Log.e("TEST", "testMagicAttackInitWithZero failed");
+    }
+
+    fun testPokemonAttackInit(){
+        val attack = fr.ablanc.qualidev.model.pokemon.Attack(10,15,"condition")
+        assert(attack.getCost() == 10)
+        assert(attack.getDamage() == 15)
+        assert(attack.getCondition() == "condition")
+        Log.d("TEST", "testMagicAttackInit done");
+    }
+
+    fun testPokemonAttackInitWithZero(){
+        try {
+            val attackWithZeroParams= fr.ablanc.qualidev.model.pokemon.Attack(0,0,"")
+        }catch (e : InitWithZeroException) {
+            Log.d("TEST", "testMagicAttackInitWithZero done")
+            return
+        }
+        Log.e("TEST", "testMagicAttackInitWithZero failed");
     }
 
     fun run(){
         testMagicAttackInit()
         testMagicAttackInitWithZero()
+        testPokemonAttackInit()
+        testPokemonAttackInitWithZero()
     }
 }
